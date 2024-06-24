73600cec58a4cb80143e4c1e3ef5b38e041428b9
Uzeda <gabrielsuzeda@gmail.com>
2024-06-24 20:48:11 -0300

    easy mode install

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Passo 1: Verificar se Docker está instalado
+if ! [ -x "$(command -v docker)" ]; then
+  echo "Erro: Docker não está instalado." >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo "Erro: Docker Compose não está instalado." >&2
+  exit 1
+fi
+
+# Passo 2: Arrumando o env para cada micro serviço
+echo "Configurando arquivos .env para cada serviço..."
+cd Adms
+cp .env.example .env
+cd ..
+
+cd Client
+cp .env.example .env
+cd ..
+
+# Passo 3: Construindo e Iniciando os Contêineres Docker
+echo "Construindo e iniciando os contêineres Docker..."
+cd Client 
+docker-compose up --build -d
+cd ../Adms
+docker-compose up --build -d
+
+# Passo 4: Instalar as dependências usando Composer
+echo "Instalando dependências do projeto..."
+docker-compose exec app-adms composer install
+docker-compose exec app-client composer install
+
+# Passo 5: Criar as tabelas no banco de dados e preencher com dados padrão
+echo "Migrando e populando o banco de dados..."
+docker-compose exec app-adms php artisan migrate
+docker-compose exec app-adms php artisan db:seed
+
+docker-compose exec app-client php artisan migrate
+docker-compose exec app-client php artisan db:seed
+
+# Passo 6: Gerar a chave secreta usada pelo JWT
+echo "Gerando chave secreta do JWT..."
+docker-compose exec app-client php artisan jwt:secret
+docker-compose exec app-adms php artisan jwt:secret
+
+echo "Configuração completa!"
