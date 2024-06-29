afc16c701abc88b1f596cae89d04fbe8f5584a1b
AndreFilho0 <filhoandre534@gmail.com>
2024-06-29 20:32:08 -0300

    colocando login para adms

--- Adms/app/Exceptions/Handler.php
+++ Adms/app/Exceptions/Handler.php
@@ -4,27 +4,41 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
+    public function report(Throwable $exception)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        parent::report($exception);
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if($exception instanceof UnauthorizedHttpException){
+            return response()->json(['error' => $exception->getMessage()], 401);
+            
+        }
+
+        if ($exception instanceof TokenExpiredException) {
+            return response()->json(['error' => 'Token has expired'], 401);
+        } elseif ($exception instanceof TokenInvalidException) {
+            return response()->json(['error' => 'Token is invalid'], 401);
+        } elseif ($exception instanceof JWTException) {
+            return response()->json(['error' => 'Token is not provided'], 401);
+        } elseif ($exception instanceof UnauthorizedHttpException && $exception->getPrevious() instanceof TokenExpiredException) {
+            return response()->json(['error' => 'Token has expired and cannot be refreshed'], 401);
+        }
+
+        return parent::render($request, $exception);
     }
 }
--- Adms/database/migrations/2014_10_12_000000_create_users_table.php
+++ Adms/database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('administradores', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean("gerente");
+            $table->boolean("professor");
+            $table->boolean("recepcionista");
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
