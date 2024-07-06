7c28fa727694f0faf36711e1bc1ee397d4e1136d
AndreFilho0 <filhoandre534@gmail.com>
2024-07-06 14:50:44 -0300

    colocando formatação da resposta http para os adms

--- Adms/app/Helpers/HttpResponse.php
+++ Adms/app/Helpers/HttpResponse.php
@@ -7,47 +7,44 @@ use App\Models\User;
 
 trait HttpResponse{
 
-    public function erroValidacao(string $messagem , int $status ,MessageBag $erros ){
+    public function erroValidacao(string $messagem , int $status ,MessageBag $erros , int $codigo){
 
         return response()->json([
            'message'=>$messagem,
            'status'=>$status,
+           'codigo' => $codigo,
            'erros'=>$erros,
         ],$status);
     }
-    public function sucesso(string $messagem , int $status ,array $erros ){
+    public function sucesso(string $messagem , int $status ,array $dados, int $codigo ){
 
         return response()->json([
            'message'=>$messagem,
+           'codigo' => $codigo,
            'status'=>$status,
-           'dados'=>$erros,
+           'dados'=>$dados,
         ],$status);
     }
-    public function sucessoAoCriarConta(string $messagem , int $status ,User $data ){
+    public function sucessoAoCriarConta(string $messagem , int $status ,User $data , int $codigo ){
 
         return response()->json([
            'message'=>$messagem,
+           'codigo' => $codigo,
            'status'=>$status,
            'dados'=>$data,
         ],$status);
     }
 
-    public function falha(string $messagem , int $status , $erros ){
+    public function falha(string $messagem , int $status , $erros , int $codigo ){
 
         return response()->json([
            'message'=>$messagem,
+           'codigo'=> $codigo,
            'status'=>$status,
            'dados do erro'=>$erros,
         ],$status);
     }
-    public function sucessoAsincrono(string $messagem , int $status , $erros ){
-
-        return response()->json([
-           'message'=>$messagem,
-           'status'=>$status,
-           'recibo para consulta'=>$erros,
-        ],$status);
-    }
+    
     
 
 }
